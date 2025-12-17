dbff0153265142b936c69a37dff3201dba78505a
tridominh-cloud <minhtri2416@gmail.com>
2025-12-18 01:34:34 +0700

    add Backgroundcolor Block

---
+++ ImageAutomate/ImageAutomate.StandardBlocks/BackgroundcolorBlock.cs
@@ -0,0 +1,156 @@
+﻿using ImageAutomate.Core;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using SixLabors.ImageSharp;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ImageAutomate.StandardBlocks;
+
+public class BackgroundcolorBlock : IBlock
+{
+    #region Fields
+
+    private readonly IReadOnlyList<Socket> _inputs = [new("Backgroundcolor.In", "Image.In")];
+    private readonly IReadOnlyList<Socket> _outputs = [new("Backgroundcolor.Out", "Image.Out")];
+
+    private bool _disposed;
+
+    private int _nodeWidth = 200;
+    private int _nodeHeight = 100;
+
+    private string _hexColor = "";
+    #endregion
+
+    #region IBlock basic
+
+    public string Name => "Backgroundcolor";
+
+    public string Title => "Backgroundcolor";
+
+    public string Content => $"Color: {HexColor}";
+
+    #endregion
+
+    #region Layout
+
+    [Category("Layout")]
+    [Description("Width of the block node")]
+    public int Width
+    {
+        get => _nodeWidth;
+        set
+        {
+            if (_nodeWidth != value)
+            {
+                _nodeWidth = value;
+                OnPropertyChanged(nameof(Width));
+            }
+        }
+    }
+
+    [Category("Layout")]
+    [Description("Height of the block node")]
+    public int Height
+    {
+        get => _nodeHeight;
+        set
+        {
+            if (_nodeHeight != value)
+            {
+                _nodeHeight = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+    }
+
+    #endregion
+
+    #region Sockets
+
+    public IReadOnlyList<Socket> Inputs => _inputs;
+    public IReadOnlyList<Socket> Outputs => _outputs;
+
+    #endregion
+
+    #region Configuration
+
+    [Category("Configuration")]
+    [Description("The color to set as the background.")]
+    public string HexColor
+    {
+        get => _hexColor;
+        set
+        {
+            if (_hexColor != value)
+            {
+                _hexColor = value;
+                OnPropertyChanged(nameof(HexColor));
+            }    
+        }
+    }
+
+    #endregion
+
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    #endregion
+
+    #region Execute
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<Socket, IReadOnlyList<IBasicWorkItem>> inputs)
+    {
+        return Execute(inputs.ToDictionary(kvp => kvp.Key.Id, kvp => kvp.Value));
+    }
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<string, IReadOnlyList<IBasicWorkItem>> inputs)
+    {
+        if (!inputs.TryGetValue(_inputs[0].Id, out var inItems))
+            throw new ArgumentException($"Input items not found for the expected input socket {_inputs[0].Id}.", nameof(inputs));
+
+        var outputItems = new List<IBasicWorkItem>();
+        Color rgbColor = new Color();
+        foreach (var sourceItem in inItems.OfType<WorkItem>())
+        {
+            rgbColor = Color.ParseHex(HexColor);
+            sourceItem.Image.Mutate(x => x.BackgroundColor(rgbColor));
+            outputItems.Add(sourceItem);
+        }
+
+        return new Dictionary<Socket, IReadOnlyList<IBasicWorkItem>>
+            {
+                { _outputs[0], outputItems }
+            };
+    }
+
+    #endregion
+
+    #region IDisposable
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+}
