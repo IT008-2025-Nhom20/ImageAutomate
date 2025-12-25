ddffe5c7cda9c99b14aeba7bb41106d116591547
Phan Long <phanmainhatlong@outlook.com>
2025-12-25 21:45:29 +0700

    Add ensemle project & update SLN file

---
+++ ImageAutomate/ImageAutomate/DashboardView.Designer.cs
@@ -0,0 +1,461 @@
+﻿namespace ImageAutomate
+{
+    partial class DashboardView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MainSplitter = new SplitContainer();
+            LeftSidebarPanel = new Panel();
+            BtnSettings = new Button();
+            BtnPluginManager = new Button();
+            BtnWorkspaces = new Button();
+            BtnWelcome = new Button();
+            MenuLabel = new Label();
+            RightContentSplit = new SplitContainer();
+            LabelSub = new Label();
+            LabelMain = new Label();
+            RecentGroup = new GroupBox();
+            PanelRecentGroup = new Panel();
+            PanelRecent5 = new Panel();
+            BtnRecent5 = new Button();
+            PanelRecent4 = new Panel();
+            BtnRecent4 = new Button();
+            PanelRecent3 = new Panel();
+            BtnRecent3 = new Button();
+            PanelRecent2 = new Panel();
+            BtnRecent2 = new Button();
+            PanelRecent1 = new Panel();
+            BtnRecent1 = new Button();
+            ButtonGroup = new GroupBox();
+            PanelStartGroup = new Panel();
+            ButtonTable = new TableLayoutPanel();
+            BtnBlankGraph = new Button();
+            BtnLoadGraph = new Button();
+            ((System.ComponentModel.ISupportInitialize)MainSplitter).BeginInit();
+            MainSplitter.Panel1.SuspendLayout();
+            MainSplitter.Panel2.SuspendLayout();
+            MainSplitter.SuspendLayout();
+            LeftSidebarPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)RightContentSplit).BeginInit();
+            RightContentSplit.Panel1.SuspendLayout();
+            RightContentSplit.Panel2.SuspendLayout();
+            RightContentSplit.SuspendLayout();
+            RecentGroup.SuspendLayout();
+            PanelRecentGroup.SuspendLayout();
+            PanelRecent5.SuspendLayout();
+            PanelRecent4.SuspendLayout();
+            PanelRecent3.SuspendLayout();
+            PanelRecent2.SuspendLayout();
+            PanelRecent1.SuspendLayout();
+            ButtonGroup.SuspendLayout();
+            PanelStartGroup.SuspendLayout();
+            ButtonTable.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MainSplitter
+            // 
+            MainSplitter.Dock = DockStyle.Fill;
+            MainSplitter.Location = new Point(0, 0);
+            MainSplitter.Name = "MainSplitter";
+            // 
+            // MainSplitter.Panel1
+            // 
+            MainSplitter.Panel1.Controls.Add(LeftSidebarPanel);
+            // 
+            // MainSplitter.Panel2
+            // 
+            MainSplitter.Panel2.Controls.Add(RightContentSplit);
+            MainSplitter.Size = new Size(800, 600);
+            MainSplitter.SplitterDistance = 59;
+            MainSplitter.TabIndex = 1;
+            // 
+            // LeftSidebarPanel
+            // 
+            LeftSidebarPanel.AutoScroll = true;
+            LeftSidebarPanel.Controls.Add(BtnSettings);
+            LeftSidebarPanel.Controls.Add(BtnPluginManager);
+            LeftSidebarPanel.Controls.Add(BtnWorkspaces);
+            LeftSidebarPanel.Controls.Add(BtnWelcome);
+            LeftSidebarPanel.Controls.Add(MenuLabel);
+            LeftSidebarPanel.Dock = DockStyle.Fill;
+            LeftSidebarPanel.Location = new Point(0, 0);
+            LeftSidebarPanel.Margin = new Padding(7);
+            LeftSidebarPanel.Name = "LeftSidebarPanel";
+            LeftSidebarPanel.Size = new Size(59, 600);
+            LeftSidebarPanel.TabIndex = 0;
+            // 
+            // BtnSettings
+            // 
+            BtnSettings.Dock = DockStyle.Top;
+            BtnSettings.Location = new Point(0, 187);
+            BtnSettings.Name = "BtnSettings";
+            BtnSettings.Size = new Size(59, 50);
+            BtnSettings.TabIndex = 4;
+            BtnSettings.Tag = "icon_mode";
+            BtnSettings.Text = "Settings";
+            BtnSettings.UseVisualStyleBackColor = true;
+            // 
+            // BtnPluginManager
+            // 
+            BtnPluginManager.Dock = DockStyle.Top;
+            BtnPluginManager.Location = new Point(0, 137);
+            BtnPluginManager.Name = "BtnPluginManager";
+            BtnPluginManager.Size = new Size(59, 50);
+            BtnPluginManager.TabIndex = 3;
+            BtnPluginManager.Tag = "icon_mode";
+            BtnPluginManager.Text = "Plugin";
+            BtnPluginManager.UseVisualStyleBackColor = true;
+            // 
+            // BtnWorkspaces
+            // 
+            BtnWorkspaces.Dock = DockStyle.Top;
+            BtnWorkspaces.Location = new Point(0, 87);
+            BtnWorkspaces.Name = "BtnWorkspaces";
+            BtnWorkspaces.Size = new Size(59, 50);
+            BtnWorkspaces.TabIndex = 2;
+            BtnWorkspaces.Tag = "icon_mode";
+            BtnWorkspaces.Text = "Workspaces";
+            BtnWorkspaces.UseVisualStyleBackColor = true;
+            // 
+            // BtnWelcome
+            // 
+            BtnWelcome.Dock = DockStyle.Top;
+            BtnWelcome.Location = new Point(0, 37);
+            BtnWelcome.Name = "BtnWelcome";
+            BtnWelcome.Size = new Size(59, 50);
+            BtnWelcome.TabIndex = 1;
+            BtnWelcome.Tag = "icon_mode";
+            BtnWelcome.Text = "Welcome";
+            BtnWelcome.UseVisualStyleBackColor = true;
+            // 
+            // MenuLabel
+            // 
+            MenuLabel.Dock = DockStyle.Top;
+            MenuLabel.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            MenuLabel.Location = new Point(0, 0);
+            MenuLabel.Margin = new Padding(5);
+            MenuLabel.Name = "MenuLabel";
+            MenuLabel.Size = new Size(59, 37);
+            MenuLabel.TabIndex = 0;
+            MenuLabel.Tag = "icon_mode";
+            MenuLabel.Text = "Menu";
+            MenuLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // RightContentSplit
+            // 
+            RightContentSplit.Dock = DockStyle.Fill;
+            RightContentSplit.IsSplitterFixed = true;
+            RightContentSplit.Location = new Point(0, 0);
+            RightContentSplit.Name = "RightContentSplit";
+            RightContentSplit.Orientation = Orientation.Horizontal;
+            // 
+            // RightContentSplit.Panel1
+            // 
+            RightContentSplit.Panel1.Controls.Add(LabelSub);
+            RightContentSplit.Panel1.Controls.Add(LabelMain);
+            // 
+            // RightContentSplit.Panel2
+            // 
+            RightContentSplit.Panel2.Controls.Add(RecentGroup);
+            RightContentSplit.Panel2.Controls.Add(ButtonGroup);
+            RightContentSplit.Size = new Size(737, 600);
+            RightContentSplit.SplitterDistance = 72;
+            RightContentSplit.TabIndex = 0;
+            // 
+            // LabelSub
+            // 
+            LabelSub.AutoSize = true;
+            LabelSub.Dock = DockStyle.Top;
+            LabelSub.Location = new Point(0, 37);
+            LabelSub.Name = "LabelSub";
+            LabelSub.Size = new Size(196, 15);
+            LabelSub.TabIndex = 1;
+            LabelSub.Text = "Visual Image Processing Flow Editor";
+            // 
+            // LabelMain
+            // 
+            LabelMain.Dock = DockStyle.Top;
+            LabelMain.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LabelMain.Location = new Point(0, 0);
+            LabelMain.Name = "LabelMain";
+            LabelMain.Size = new Size(737, 37);
+            LabelMain.TabIndex = 0;
+            LabelMain.Text = "ImageAutomate";
+            // 
+            // RecentGroup
+            // 
+            RecentGroup.Controls.Add(PanelRecentGroup);
+            RecentGroup.Dock = DockStyle.Top;
+            RecentGroup.Location = new Point(0, 129);
+            RecentGroup.Name = "RecentGroup";
+            RecentGroup.Size = new Size(737, 129);
+            RecentGroup.TabIndex = 5;
+            RecentGroup.TabStop = false;
+            RecentGroup.Text = "Recent Workspaces";
+            // 
+            // PanelRecentGroup
+            // 
+            PanelRecentGroup.AutoScroll = true;
+            PanelRecentGroup.Controls.Add(PanelRecent5);
+            PanelRecentGroup.Controls.Add(PanelRecent4);
+            PanelRecentGroup.Controls.Add(PanelRecent3);
+            PanelRecentGroup.Controls.Add(PanelRecent2);
+            PanelRecentGroup.Controls.Add(PanelRecent1);
+            PanelRecentGroup.Dock = DockStyle.Fill;
+            PanelRecentGroup.Location = new Point(3, 19);
+            PanelRecentGroup.Name = "PanelRecentGroup";
+            PanelRecentGroup.Size = new Size(731, 107);
+            PanelRecentGroup.TabIndex = 2;
+            // 
+            // PanelRecent5
+            // 
+            PanelRecent5.AutoSize = true;
+            PanelRecent5.Controls.Add(BtnRecent5);
+            PanelRecent5.Dock = DockStyle.Left;
+            PanelRecent5.Location = new Point(592, 0);
+            PanelRecent5.Name = "PanelRecent5";
+            PanelRecent5.Size = new Size(148, 90);
+            PanelRecent5.TabIndex = 6;
+            // 
+            // BtnRecent5
+            // 
+            BtnRecent5.AutoSize = true;
+            BtnRecent5.Dock = DockStyle.Fill;
+            BtnRecent5.Location = new Point(0, 0);
+            BtnRecent5.Name = "BtnRecent5";
+            BtnRecent5.Size = new Size(148, 90);
+            BtnRecent5.TabIndex = 0;
+            BtnRecent5.Text = "button11";
+            BtnRecent5.UseVisualStyleBackColor = true;
+            // 
+            // PanelRecent4
+            // 
+            PanelRecent4.AutoSize = true;
+            PanelRecent4.Controls.Add(BtnRecent4);
+            PanelRecent4.Dock = DockStyle.Left;
+            PanelRecent4.Location = new Point(444, 0);
+            PanelRecent4.Name = "PanelRecent4";
+            PanelRecent4.Size = new Size(148, 90);
+            PanelRecent4.TabIndex = 5;
+            // 
+            // BtnRecent4
+            // 
+            BtnRecent4.AutoSize = true;
+            BtnRecent4.Dock = DockStyle.Fill;
+            BtnRecent4.Location = new Point(0, 0);
+            BtnRecent4.Name = "BtnRecent4";
+            BtnRecent4.Size = new Size(148, 90);
+            BtnRecent4.TabIndex = 0;
+            BtnRecent4.Text = "button10";
+            BtnRecent4.UseVisualStyleBackColor = true;
+            // 
+            // PanelRecent3
+            // 
+            PanelRecent3.AutoSize = true;
+            PanelRecent3.Controls.Add(BtnRecent3);
+            PanelRecent3.Dock = DockStyle.Left;
+            PanelRecent3.Location = new Point(296, 0);
+            PanelRecent3.Name = "PanelRecent3";
+            PanelRecent3.Size = new Size(148, 90);
+            PanelRecent3.TabIndex = 4;
+            // 
+            // BtnRecent3
+            // 
+            BtnRecent3.AutoSize = true;
+            BtnRecent3.Dock = DockStyle.Fill;
+            BtnRecent3.Location = new Point(0, 0);
+            BtnRecent3.Name = "BtnRecent3";
+            BtnRecent3.Size = new Size(148, 90);
+            BtnRecent3.TabIndex = 0;
+            BtnRecent3.Text = "button9";
+            BtnRecent3.UseVisualStyleBackColor = true;
+            // 
+            // PanelRecent2
+            // 
+            PanelRecent2.AutoSize = true;
+            PanelRecent2.Controls.Add(BtnRecent2);
+            PanelRecent2.Dock = DockStyle.Left;
+            PanelRecent2.Location = new Point(148, 0);
+            PanelRecent2.Name = "PanelRecent2";
+            PanelRecent2.Size = new Size(148, 90);
+            PanelRecent2.TabIndex = 3;
+            // 
+            // BtnRecent2
+            // 
+            BtnRecent2.AutoSize = true;
+            BtnRecent2.Dock = DockStyle.Fill;
+            BtnRecent2.Location = new Point(0, 0);
+            BtnRecent2.Name = "BtnRecent2";
+            BtnRecent2.Size = new Size(148, 90);
+            BtnRecent2.TabIndex = 0;
+            BtnRecent2.Text = "button8";
+            BtnRecent2.UseVisualStyleBackColor = true;
+            // 
+            // PanelRecent1
+            // 
+            PanelRecent1.AutoSize = true;
+            PanelRecent1.Controls.Add(BtnRecent1);
+            PanelRecent1.Dock = DockStyle.Left;
+            PanelRecent1.Location = new Point(0, 0);
+            PanelRecent1.Name = "PanelRecent1";
+            PanelRecent1.Size = new Size(148, 90);
+            PanelRecent1.TabIndex = 2;
+            // 
+            // BtnRecent1
+            // 
+            BtnRecent1.AutoSize = true;
+            BtnRecent1.Dock = DockStyle.Fill;
+            BtnRecent1.Location = new Point(0, 0);
+            BtnRecent1.Name = "BtnRecent1";
+            BtnRecent1.Size = new Size(148, 90);
+            BtnRecent1.TabIndex = 0;
+            BtnRecent1.Text = "button7";
+            BtnRecent1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonGroup
+            // 
+            ButtonGroup.Controls.Add(PanelStartGroup);
+            ButtonGroup.Dock = DockStyle.Top;
+            ButtonGroup.Location = new Point(0, 0);
+            ButtonGroup.Name = "ButtonGroup";
+            ButtonGroup.Size = new Size(737, 129);
+            ButtonGroup.TabIndex = 4;
+            ButtonGroup.TabStop = false;
+            ButtonGroup.Text = "Start";
+            // 
+            // PanelStartGroup
+            // 
+            PanelStartGroup.AutoScroll = true;
+            PanelStartGroup.Controls.Add(ButtonTable);
+            PanelStartGroup.Dock = DockStyle.Fill;
+            PanelStartGroup.Location = new Point(3, 19);
+            PanelStartGroup.Name = "PanelStartGroup";
+            PanelStartGroup.Size = new Size(731, 107);
+            PanelStartGroup.TabIndex = 2;
+            // 
+            // ButtonTable
+            // 
+            ButtonTable.ColumnCount = 2;
+            ButtonTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            ButtonTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            ButtonTable.Controls.Add(BtnBlankGraph, 0, 0);
+            ButtonTable.Controls.Add(BtnLoadGraph, 1, 0);
+            ButtonTable.Dock = DockStyle.Top;
+            ButtonTable.Location = new Point(0, 0);
+            ButtonTable.Name = "ButtonTable";
+            ButtonTable.RowCount = 1;
+            ButtonTable.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            ButtonTable.Size = new Size(731, 100);
+            ButtonTable.TabIndex = 3;
+            // 
+            // BtnBlankGraph
+            // 
+            BtnBlankGraph.Dock = DockStyle.Fill;
+            BtnBlankGraph.Location = new Point(3, 3);
+            BtnBlankGraph.Name = "BtnBlankGraph";
+            BtnBlankGraph.Size = new Size(359, 94);
+            BtnBlankGraph.TabIndex = 3;
+            BtnBlankGraph.Text = "Blank Graph";
+            BtnBlankGraph.UseVisualStyleBackColor = true;
+            // 
+            // BtnLoadGraph
+            // 
+            BtnLoadGraph.Dock = DockStyle.Fill;
+            BtnLoadGraph.Location = new Point(368, 3);
+            BtnLoadGraph.Name = "BtnLoadGraph";
+            BtnLoadGraph.Size = new Size(360, 94);
+            BtnLoadGraph.TabIndex = 1;
+            BtnLoadGraph.Text = "Load Graph";
+            BtnLoadGraph.UseVisualStyleBackColor = true;
+            // 
+            // DashboardView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(MainSplitter);
+            Name = "DashboardView";
+            Size = new Size(800, 600);
+            MainSplitter.Panel1.ResumeLayout(false);
+            MainSplitter.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)MainSplitter).EndInit();
+            MainSplitter.ResumeLayout(false);
+            LeftSidebarPanel.ResumeLayout(false);
+            RightContentSplit.Panel1.ResumeLayout(false);
+            RightContentSplit.Panel1.PerformLayout();
+            RightContentSplit.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)RightContentSplit).EndInit();
+            RightContentSplit.ResumeLayout(false);
+            RecentGroup.ResumeLayout(false);
+            PanelRecentGroup.ResumeLayout(false);
+            PanelRecentGroup.PerformLayout();
+            PanelRecent5.ResumeLayout(false);
+            PanelRecent5.PerformLayout();
+            PanelRecent4.ResumeLayout(false);
+            PanelRecent4.PerformLayout();
+            PanelRecent3.ResumeLayout(false);
+            PanelRecent3.PerformLayout();
+            PanelRecent2.ResumeLayout(false);
+            PanelRecent2.PerformLayout();
+            PanelRecent1.ResumeLayout(false);
+            PanelRecent1.PerformLayout();
+            ButtonGroup.ResumeLayout(false);
+            PanelStartGroup.ResumeLayout(false);
+            ButtonTable.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer MainSplitter;
+        private Panel LeftSidebarPanel;
+        private Button BtnSettings;
+        private Button BtnPluginManager;
+        private Button BtnWorkspaces;
+        private Button BtnWelcome;
+        private Label MenuLabel;
+        private SplitContainer RightContentSplit;
+        private Label LabelSub;
+        private Label LabelMain;
+        private GroupBox ButtonGroup;
+        private Panel PanelStartGroup;
+        private TableLayoutPanel ButtonTable;
+        private Button BtnBlankGraph;
+        private Button BtnLoadGraph;
+        private GroupBox RecentGroup;
+        private Panel PanelRecentGroup;
+        private Panel PanelRecent5;
+        private Button BtnRecent5;
+        private Panel PanelRecent4;
+        private Button BtnRecent4;
+        private Panel PanelRecent3;
+        private Button BtnRecent3;
+        private Panel PanelRecent2;
+        private Button BtnRecent2;
+        private Panel PanelRecent1;
+        private Button BtnRecent1;
+    }
+}
---
+++ ImageAutomate/ImageAutomate/Main.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ImageAutomate
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dashboardView1 = new DashboardView();
+            SuspendLayout();
+            // 
+            // dashboardView1
+            // 
+            dashboardView1.Dock = DockStyle.Fill;
+            dashboardView1.Location = new Point(0, 0);
+            dashboardView1.Name = "dashboardView1";
+            dashboardView1.Size = new Size(800, 450);
+            dashboardView1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dashboardView1);
+            Name = "Main";
+            Text = "Main";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DashboardView dashboardView1;
+    }
+}
