825b021a1d0f918e6d28a0988d1ef02f61c890c7
tridominh-cloud <minhtri2416@gmail.com>
2025-12-26 21:20:44 +0700

    add drag and drop

--- ImageAutomate/ImageAutomate/Views/EditorView.Designer.cs
+++ ImageAutomate/ImageAutomate/Views/EditorView.Designer.cs
@@ -28,17 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem1 = new ToolStripMenuItem();
+            splitContainer1 = new SplitContainer();
+            groupBox1 = new GroupBox();
+            toolListBox = new ListBox();
+            graphRenderPanel1 = new ImageAutomate.UI.GraphRenderPanel();
+            button1 = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            graphRenderPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(730, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openToolStripMenuItem, closeToolStripMenuItem, saveToolStripMenuItem, saveToolStripMenuItem1 });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.Size = new Size(103, 22);
+            newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(103, 22);
+            openToolStripMenuItem.Text = "Open";
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(103, 22);
+            closeToolStripMenuItem.Text = "Save";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(103, 22);
+            saveToolStripMenuItem.Text = "Close";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            saveToolStripMenuItem1.Size = new Size(103, 22);
+            saveToolStripMenuItem1.Text = "Exit";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 24);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(graphRenderPanel1);
+            splitContainer1.Size = new Size(730, 576);
+            splitContainer1.SplitterDistance = 121;
+            splitContainer1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(toolListBox);
+            groupBox1.Dock = DockStyle.Fill;
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(121, 576);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tools";
+            // 
+            // toolListBox
+            // 
+            toolListBox.Dock = DockStyle.Fill;
+            toolListBox.FormattingEnabled = true;
+            toolListBox.Location = new Point(3, 19);
+            toolListBox.Name = "toolListBox";
+            toolListBox.Size = new Size(115, 554);
+            toolListBox.TabIndex = 0;
+            toolListBox.MouseDown += toolListBox_MouseDown;
+            // 
+            // graphRenderPanel1
+            // 
+            graphRenderPanel1.AllowDrop = true;
+            graphRenderPanel1.BackColor = Color.White;
+            graphRenderPanel1.Controls.Add(button1);
+            graphRenderPanel1.Dock = DockStyle.Fill;
+            graphRenderPanel1.Location = new Point(0, 0);
+            graphRenderPanel1.Name = "graphRenderPanel1";
+            graphRenderPanel1.Size = new Size(605, 576);
+            graphRenderPanel1.TabIndex = 0;
+            graphRenderPanel1.DragDrop += graphRenderPanel1_DragDrop;
+            graphRenderPanel1.DragEnter += graphRenderPanel1_DragEnter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(152, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // EditorView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(splitContainer1);
+            Controls.Add(menuStrip1);
             Name = "EditorView";
-            Size = new Size(800, 600);
+            Size = new Size(730, 600);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            graphRenderPanel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private SplitContainer splitContainer1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem1;
+        private GroupBox groupBox1;
+        private ListBox toolListBox;
+        private UI.GraphRenderPanel graphRenderPanel1;
+        private Button button1;
     }
 }
--- ImageAutomate/ImageAutomate/Views/EditorView.cs
+++ ImageAutomate/ImageAutomate/Views/EditorView.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Accessibility;
+using ImageAutomate.Core;
+using ImageAutomate.StandardBlocks;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +19,42 @@ namespace ImageAutomate.Views
         public EditorView()
         {
             InitializeComponent();
+            toolListBox.Items.AddRange([typeof(BrightnessBlock)]);
+        }
+
+        private void toolListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lb = toolListBox;
+            Point pt = new Point(e.X, e.Y);
+            int index = lb.IndexFromPoint(pt);
+
+            if (index >= 0)
+            {
+                lb.DoDragDrop(lb.Items[index], DragDropEffects.Copy);
+            }
+        }
+
+        private void graphRenderPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Type)))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void graphRenderPanel1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Type)))
+            {
+                var type = (Type)e.Data.GetData(typeof(Type));
+                var block = (IBlock)Activator.CreateInstance(type);
+                graphRenderPanel1.Invalidate();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var block = new BrightnessBlock();
+            graphRenderPanel1.AddPredecessor(block, new Socket("",""), new Socket("",""));
         }
     }
 }
